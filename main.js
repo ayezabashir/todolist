2135b4a785be88449db48e10761a13482e3e9f62
Ayeza <ayezabashir442@gmail.com>
2023-04-06 22:59:39 +0500

    saving to do

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
+// selecting elements
 const form = document.getElementById("todoform");
+const todoInput = document.getElementById("newtodo");
+
+// saving the todos in array
+const todos = [];
+
+// form submission
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    console.log("submit");
+    saveToDo();
 })
+
+// function to add todo
+function saveToDo() {
+    const todoValue = todoInput.value;
+    const isEmpty = todoValue === "";
+    const isDuplicate = todos.some(
+        (todo) => todo.value.toUpperCase() === todoValue.toUpperCase()
+    );
+
+    if (isEmpty) {
+        alert('empty value can not be added');
+    } else if (isDuplicate) {
+        alert('duplicates cannot be added');
+    }
+    else {
+        const todo = {
+            value: todoValue,
+            checked: false,
+            color: '#' + Math.floor(Math.random() * 16777215).toString(16)
+        }
+        todos.push(todo);
+        todoInput.value = '';
+        console.log(todos);
+    }
+}
